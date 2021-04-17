95c731c55ad8050933e7eb0c3e296c421880ca44
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-04-17 16:24:22 -0400

    code working when debugging. otherwise get error. must debug

--- intellij_tests/collecting_tweets_v4_pagination/src/main/java/FullArchiveSearchDemo.java
+++ intellij_tests/collecting_tweets_v4_pagination/src/main/java/FullArchiveSearchDemo.java
@@ -26,42 +26,50 @@ import org.json.simple.parser.ParseException;
  * */
 public class FullArchiveSearchDemo {
 
-    private static String start_time = "2020-01-01T00:00:00.000Z";
-    private static String end_time = "2020-02-01T02:00:00.000Z";
+    private static String start_time = "2020-02-01T00:00:00.000Z";
+    private static String end_time = "2020-03-01T02:00:00.000Z";
+    private static String next_token = null;
+    private static String bearerToken = System.getenv("BEARER_TOKEN");
     // To set your enviornment variables in your terminal run the following line:
     // export 'BEARER_TOKEN'='<your_bearer_token>'
 
     public static void main(String args[]) throws IOException, URISyntaxException, ParseException {
-        String bearerToken = System.getenv("BEARER_TOKEN");
         if (null != bearerToken) {
-            //Replace the search term with a term of your choice
-//            String response = search("from:TwitterDev OR from:SnowBotDev OR from:DailyNASA", bearerToken);
-//            String response = search("(FSLR) OR (TetraSun) OR (Mark Widmar) lang:en", bearerToken);
-            String response = search("\"$ENPH\" lang:en", bearerToken);
-//            String response = search("NYU", bearerToken);
+            String response = search("$ENPH lang:en", bearerToken);
             System.out.println(response);
-            save_to_csv(response);
 
+            save_response_to_csv(response);
+            // request more pages if next_token is available
+            while (next_token != null) {
+                System.out.println("next_token= " + next_token);
+                response = search("$ENPH lang:en", bearerToken);
+                save_response_to_csv(response);
+            }
+            next_token = null;
+            System.out.println("No token!!");
         } else {
             System.out.println("There was a problem getting your bearer token. Please make sure you set the BEARER_TOKEN environment variable");
         }
     }
 
-    private static void save_to_csv(String response) throws ParseException, IOException {
+    private static void save_response_to_csv(String response) throws ParseException, IOException, URISyntaxException {
         JSONParser parse = new JSONParser();
-        JSONObject jobj = (JSONObject)parse.parse(response);
-        JSONArray jsonarr_1 = (JSONArray) jobj.get("data");
-        JSONObject meta = (JSONObject) jobj.get("meta");
-        System.out.println(jsonarr_1);
+        JSONObject response_json = (JSONObject)parse.parse(response);
+        JSONArray data = (JSONArray) response_json.get("data");
+        JSONObject meta = (JSONObject) response_json.get("meta");
+        System.out.println(data);
         System.out.println(meta);
-        if (meta.containsKey("next_token")){
-            String next_token = meta.get("next_token").toString();
-            System.out.println("next_token= " + next_token);
-        } else {
-            System.out.println("No token!!");
-        }
-        //Get data for Results array
 
+        if (meta.containsKey("next_token")) {
+            next_token = meta.get("next_token").toString();
+        } else {
+            next_token = null;
+        }
+
+        save_tweets_to_csv(data);
+    }
+
+    private static void save_tweets_to_csv(JSONArray data) throws IOException {
         FileWriter csvWriter = new FileWriter("/Users/Jroldan001/nyu/spring_2021/bds/bds_project_workspace/intellij_tests/collecting_tweets_v4_pagination/data_collected/enph_v4_pagination.csv",true);// change to relative path later
         csvWriter.append("CreatedAt");
         csvWriter.append(",");
@@ -69,20 +77,19 @@ public class FullArchiveSearchDemo {
         csvWriter.append(",");
         csvWriter.append("TweetText");
         csvWriter.append("\n");
-        for(int i=0;i<jsonarr_1.size();i++) {
-            //Store the JSON objects in an array
-            //Get the index of the JSON object and print the values as per the index
-            JSONObject jsonobj_1 = (JSONObject)jsonarr_1.get(i);
+        for(int i=0;i<data.size();i++) {
+            JSONObject tweet = (JSONObject)data.get(i);
 //            System.out.println("Elements under results array");
-            csvWriter.append(jsonobj_1.get("created_at").toString());
+            csvWriter.append(tweet.get("created_at").toString());
             csvWriter.append(",");
-            csvWriter.append(jsonobj_1.get("id").toString());
+            csvWriter.append(tweet.get("id").toString());
             csvWriter.append(",");
-            csvWriter.append(jsonobj_1.get("text").toString().replace("\n", "").replace("\r", "").replace(",", " "));
+            csvWriter.append(tweet.get("text").toString().replace("\n", "").replace("\r", "").replace(",", " "));
             csvWriter.append("\n");
         }
         csvWriter.flush();
         csvWriter.close();
+
     }
 
     /*
@@ -106,6 +113,9 @@ public class FullArchiveSearchDemo {
 //        queryParameters.add(new BasicNameValuePair("end_time", "2020-02-08T11:30:00.000Z"));
         queryParameters.add(new BasicNameValuePair("start_time", start_time));
         queryParameters.add(new BasicNameValuePair("end_time", end_time));
+        if (next_token != null){
+            queryParameters.add(new BasicNameValuePair("next_token", next_token));
+        }
 
 //        queryParameters.add(new BasicNameValuePair("lang", "en"));
         uriBuilder.addParameters(queryParameters);
