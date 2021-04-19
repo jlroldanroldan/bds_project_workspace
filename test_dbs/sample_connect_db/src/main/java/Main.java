73c8bbd53a7c5cc1d88f8ac90fca86ab028a0599
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2021-04-19 13:48:58 -0400

    Able to connect mysql db

--- test_dbs/sample_connect_db/src/main/java/Main.java
+++ test_dbs/sample_connect_db/src/main/java/Main.java
@@ -1,3 +1,20 @@
-public class Main {
+import java.sql.*;
 
+public class Main {
+    public static void main(String[] args){
+        String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+        String user = "root";
+        String password = "jotajota24";
+        try {
+            Connection myConn = DriverManager.getConnection(url, user, password);
+            Statement myStmt = myConn.createStatement();
+            String sql = "SELECT * FROM testing_enphase_tweets.enphase_energy_tweets_small ";
+            ResultSet rs = myStmt.executeQuery(sql);
+            while (rs.next()){
+                System.out.println(rs.getString("TweetText"));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
